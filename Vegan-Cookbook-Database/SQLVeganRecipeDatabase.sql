173e3b9162abf083409dcbe2247728fde82ac093
Cami63 <johnscamryn@gmail.com>
2021-01-29 22:48:29 -0500

    AddedSomeTables

--- Vegan-Cookbook-Database/SQLVeganRecipeDatabase.sql
+++ Vegan-Cookbook-Database/SQLVeganRecipeDatabase.sql
@@ -9,17 +9,30 @@ CREATE TABLE recipes (
 );
 CREATE TABLE ingredients (
     id BIGINT NOT NULL AUTO_INCREMENT, 
-    ingredient_name varchar(255) NOT NULL, 
-    recipe_id INT NOT NULL, 
+    ingredient_name varchar(255) NOT NULL UNIQUE, 
     PRIMARY KEY (id), 
-    CONSTRAINT ingredients_recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+);
+CREATE TABLE ingredients_recipes (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    recipe_id BIGINT NOT NULL,
+    ingredient_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes (id),
+    CONSTRAINT ingredient_id_fk FOREIGN KEY (ingredient_id) REFERENCES ingredients (id)
 );
 CREATE TABLE meal_types (
     id INT NOT NULL AUTO_INCREMENT, 
-    meal_name varchar(31) NOT NULL,
-    recipe_id INT NOT NULL, 
+    meal_name varchar(31) NOT NULL UNIQUE,
     PRIMARY KEY (id), 
     CONSTRAINT meal_types_recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+);
+CREATE TABLE meal_recipes (
+    id INT NOT NULL AUTO_INCREMENT,
+    meal_id BIGINT NOT NULL,
+    recipe_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT meal_id_fk FOREIGN KEY (meal_id) REFERENCES meal_types (id),
+    CONSTRAINT recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes (id)
 );
 CREATE TABLE recipe_steps (
     id BIGINT NOT NULL AUTO_INCREMENT, 
