d55480881f2283e5ae30bf7703a4203fa64c72bc
Cami63 <johnscamryn@gmail.com>
2021-02-10 15:35:20 -0500

    I have updated the SQL using Workbench so now the tables have been created

--- Vegan-Cookbook-Database/SQLVeganRecipeDatabase.sql
+++ Vegan-Cookbook-Database/SQLVeganRecipeDatabase.sql
@@ -1,44 +1,55 @@
-CREATE TABLE recipes (
+CREATE DATABASE vegan_cookbook;
+
+USE vegan_cookbook;
+
+CREATE SCHEMA veg;
+
+CREATE TABLE veg.recipes (
     id BIGINT NOT NULL AUTO_INCREMENT, 
     recipe_name varchar(255), 
     health_rating INT NOT NULL, 
     nationality INT, 
     prep_time_minutes INT NOT NULL, 
-    PRIMARY KEY (recipe_id)
-    CONSTRAINT health_rating_ck CHECK ((health_rating >=1 AND health_rating <=5))
+    PRIMARY KEY (id),
+    CONSTRAINT recipes_health_rating_ck CHECK ((health_rating >=1 AND health_rating <=5))
 );
-CREATE TABLE ingredients (
+CREATE TABLE veg.ingredients (
     id BIGINT NOT NULL AUTO_INCREMENT, 
     ingredient_name varchar(255) NOT NULL UNIQUE, 
-    PRIMARY KEY (id), 
+    PRIMARY KEY (id) 
 );
-CREATE TABLE ingredients_recipes (
+
+CREATE TABLE veg.ingredients_recipes (
     id BIGINT NOT NULL AUTO_INCREMENT,
     recipe_id BIGINT NOT NULL,
     ingredient_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes (id),
-    CONSTRAINT ingredient_id_fk FOREIGN KEY (ingredient_id) REFERENCES ingredients (id)
+    CONSTRAINT ingredients_recipes_recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes (id),
+    CONSTRAINT ingredients_recipes_ingredient_id_fk FOREIGN KEY (ingredient_id) REFERENCES ingredients (id)
 );
-CREATE TABLE meal_types (
+
+CREATE TABLE veg.meal_types (
     id INT NOT NULL AUTO_INCREMENT, 
     meal_name varchar(31) NOT NULL UNIQUE,
+    recipe_id BIGINT NOT NULL,
     PRIMARY KEY (id), 
     CONSTRAINT meal_types_recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes(id)
 );
-CREATE TABLE meal_recipes (
-    id INT NOT NULL AUTO_INCREMENT,
-    meal_id BIGINT NOT NULL,
+
+CREATE TABLE veg.meal_recipes (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    meal_id INT NOT NULL,
     recipe_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT meal_id_fk FOREIGN KEY (meal_id) REFERENCES meal_types (id),
-    CONSTRAINT recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes (id)
+    CONSTRAINT meal_recipes_meal_id_fk FOREIGN KEY (meal_id) REFERENCES meal_types (id),
+    CONSTRAINT meal_recipes_recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes (id)
 );
-CREATE TABLE recipe_steps (
+
+CREATE TABLE veg.recipe_steps (
     id BIGINT NOT NULL AUTO_INCREMENT, 
     step_number INT NOT NULL,
     step_description varchar(1023), 
-    recipe_id INT NOT NULL, 
+    recipe_id BIGINT NOT NULL, 
     PRIMARY KEY (id), 
     CONSTRAINT recipe_steps_recipe_id_fk FOREIGN KEY (recipe_id) REFERENCES recipes(id)
 );
